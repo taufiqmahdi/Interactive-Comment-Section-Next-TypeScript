df742654b35c4620690449465f64b5749d1c7012
taufiqmahdi <57888672+taufiqmahdi@users.noreply.github.com>
2023-09-01 15:35:58 +0700

    Fix Linting
    - Fix Linting

--- src/app/components/DeleteModal.tsx
+++ src/app/components/DeleteModal.tsx
@@ -35,7 +35,7 @@ const DeleteModal = (props: DeleteModalProps) => {
     <div className='fixed top-0 right-0 bg-black bg-opacity-30 p-4 h-screen w-screen flex items-center justify-center'>
         <div className='z-40 bg-neutral-white flex flex-col gap-4 p-8 rounded-lg md:w-96'>
             <div className='font-medium text-neutral-dark-blue text-xl'>Delete comment?</div>
-            <div className='text-neutral-grayish-blue'>Are you sure you want to delete this comment? This will remove the comment and can't be undone</div>
+            <div className='text-neutral-grayish-blue'>Are you sure you want to delete this comment? This will remove the comment and cant be undone</div>
             <div className='flex gap-4 justify-between'>
                 <button onClick={handleClick} className='px-4 py-3 md:px-6 md:py-4 uppercase bg-neutral-grayish-blue text-neutral-white font-medium rounded-lg'>No, cancel</button>
                 <button onClick={handleDeleteClick} className='px-4 py-3 md:px-6 md:py-4 uppercase bg-primary-soft-red text-neutral-white font-medium rounded-lg'>Yes, delete</button>
